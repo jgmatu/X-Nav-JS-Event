0410ba5348dc7ea33603372a7ea54c7f00d57a4a
Javier <fj.gutierrezs91@gmail.com>
2017-02-26 00:34:17 +0100

    Resolved with debugger

--- form-5.js
+++ form-5.js
@@ -1,5 +1,3 @@
-//window.addEventListener('load', setup);
-
 var setup = function() {
       var input = document.getElementById("input");
       input.addEventListener('input', changeColor, false);
@@ -9,4 +7,3 @@ var changeColor = function (e) {
       var div = document.getElementById("color");
       div.style.backgroundColor = this.value;
 }
-setup();
